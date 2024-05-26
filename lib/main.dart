51dcf26b5b984cd08b624fddecbb2687aadcd15e
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-05-26 11:19:35 +0800

    ♻️ refactor: Simplify main entry point of the application
    This commit simplifies the main entry point of the application by removing the unnecessary `DependencyInjection` initialization and Firebase setup. The Flutter binding is still ensured to be initialized before running the app, and the `ProviderScope` widget is used for dependency injection. This change improves the code readability and reduces unnecessary complexity.

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:payroll_system/src/app.dart';
-import 'package:payroll_system/src/utils/dependency_injection.dart';
 
-/// The main entry point of the application.
-/// Initializes Firebase and runs the main app.
+/// The entry point of the application.
+///
+/// This function initializes the Flutter binding and runs the main application.
+/// It ensures that the Flutter framework is initialized before running the app.
+/// The [ProviderScope] widget is used to provide dependency injection using the
+/// Provider package. The [MainApp] widget is the root of the application.
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await DependencyInjection().initFirebase();
-
-  runApp(
-    const ProviderScope(child: MainApp()),
-  );
+  runApp(const ProviderScope(child: MainApp()));
 }
