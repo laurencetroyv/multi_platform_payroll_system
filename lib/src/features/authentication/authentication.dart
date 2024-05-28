50d83e68c6004f19aa0e91cd6de432c8044ba29d
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-05-29 01:27:21 +0800

    ✨ feat: Add authentication widgets for sign-in, sign-up and welcome screen

---
+++ lib/src/features/authentication/authentication.dart
@@ -0,0 +1,4 @@
+export 'entities/entities.dart';
+export 'providers/providers.dart';
+export 'screens/screens.dart';
+export 'widgets/widgets.dart';
---
+++ lib/src/features/authentication/widgets/widgets.dart
@@ -0,0 +1,7 @@
+export 'sign_in/mobile_view.dart';
+export 'sign_in/web_view.dart';
+export 'sign_in/sign_in_btn.dart';
+
+export 'sign_up/mobile_view.dart';
+export 'sign_up/web_view.dart';
+export 'sign_up/sign_up_bnt.dart';
