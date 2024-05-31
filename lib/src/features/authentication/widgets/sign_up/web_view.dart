0789a02407f23d0482d5a966bf29ea9837d2a448
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-05-31 14:32:50 +0800

    🚑 fix: unable to sign in due to employee ID 2

--- lib/src/features/authentication/widgets/sign_up/web_view.dart
+++ lib/src/features/authentication/widgets/sign_up/web_view.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -27,8 +28,10 @@ class _WebViewState extends ConsumerState<SignUpWebView> {
 
   @override
   Widget build(BuildContext context) {
-    final isMobile = MediaQuery.of(context).size.width < 400 ||
-        (Platform.isAndroid || Platform.isIOS);
+    final isMobile = kIsWeb
+        ? false
+        : MediaQuery.of(context).size.width < 400 ||
+            (Platform.isAndroid || Platform.isIOS);
 
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
