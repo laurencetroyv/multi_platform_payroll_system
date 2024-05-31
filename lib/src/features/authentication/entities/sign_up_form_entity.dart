5d7b8076fa3735bc23bad4ee77a3bb75a4454d8d
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-05-31 10:23:40 +0800

    ✨ feat: Add employeeId field to SignUpFormEntity's toJson method

--- lib/src/features/authentication/entities/sign_up_form_entity.dart
+++ lib/src/features/authentication/entities/sign_up_form_entity.dart
@@ -6,16 +6,22 @@ class SignUpFormEntity {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController employeeId = TextEditingController();
 
   String get name => nameController.text;
   String get email => emailController.text;
   String get password => passwordController.text;
+  String get id => employeeId.text;
 
-  Map<String, String> toJson() {
+  String get employerRole => UserRoles.employer.toString();
+  String get employeeRole => UserRoles.employee.toString();
+
+  Map<String, String> toJson({String? name}) {
     return {
-      'name': name,
+      'name': this.name.isNotEmpty ? this.name : name!,
       'email': email,
-      'role': UserRoles.employer.toString(),
+      'role': id.isNotEmpty ? employeeRole : employerRole,
+      'employeeId': id,
     };
   }
 }
